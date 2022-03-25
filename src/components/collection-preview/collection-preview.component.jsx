e3193733aff46d567c51bb19e89db2771e72ddc0
studiomac-152 <studiomac-152@studiomac-152s-iMac.local>
2022-03-25 17:51:32 +0530

    cart icon show, showed that cart or checkout number on cart icon

--- src/components/collection-preview/collection-preview.component.jsx
+++ src/components/collection-preview/collection-preview.component.jsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import { withRouter } from 'react-router-dom';
 
-import CollectionItem from '../collection-item/collection-item.component';
+import {default as CollectionItem} from '../collection-item/collection-item.container';
 
 import './collection-preview.styles.scss';
 
@@ -15,7 +15,7 @@ const CollectionPreview = ({ title, items, history, match }) => (
     </h1>
     <div className='preview'>
       {items
-        .filter((item, idx) => idx < 4)
+        .filter((items, idx) => idx < 4)
         .map(item => (
           <CollectionItem key={item.id} item={item} />
         ))}
--- src/index.js
+++ src/index.js
@@ -3,19 +3,53 @@ import ReactDOM from 'react-dom';
 import { BrowserRouter } from 'react-router-dom';
 import { Provider } from 'react-redux';
 import { PersistGate } from 'redux-persist/integration/react';
+import { ApolloProvider } from 'react-apollo';
+import { createHttpLink } from 'apollo-link-http';
+import { InMemoryCache } from 'apollo-cache-inmemory';
+import { ApolloClient } from 'apollo-boost';
+//gql i s a function of apollo boost
 
 import { store, persistor } from './redux/store';
+import { resolvers, typeDefs } from './graphql/resolvers';
 
 import './index.css';
 import App from './App';
 
+//create link for our new graphQL method
+
+const httpLink = createHttpLink({
+  uri: 'https://crwn-clothing.com'
+});
+
+//for cache that memory for local storage application
+
+const cache = new InMemoryCache();
+
+//make the client what we request for and our cache memory
+
+const client = new ApolloClient({
+  link: httpLink,
+  cache,
+  resolvers,
+  typeDefs
+});
+
+client.writeData({
+  data: {
+    cartHidden: true,
+    cartItems: []
+  }
+});
+
 ReactDOM.render(
+  <ApolloProvider client={client}>
   <Provider store={store}>
     <BrowserRouter>
       <PersistGate persistor={persistor}>
         <App />
       </PersistGate>
     </BrowserRouter>
-  </Provider>,
+  </Provider>
+  </ApolloProvider>,
   document.getElementById('root')
 );
